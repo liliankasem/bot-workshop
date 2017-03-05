9af7a24db0031bc79a9be3cec163a9f09b83da40
Lilian Kasem <liliankasem@gmail.com>
2017-03-05 14:23:12 +0000

    Clean up and adding final csharp project

---
+++ final/csharp/MyFirstBot/MyFirstBot/TravelApp/LUISApp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace MyFirstBot.TravelApp
+{
+
+	[LuisModel("YOUR_LUIS_MODEL_ID", "YOUR_LUIS_KEY")]
+	[Serializable]
+	public class LUISApp : LuisDialog<object>
+	{
+		//Will run when a None intent is Triggered
+		[LuisIntent("")]
+		public async Task None(IDialogContext context, LuisResult result)
+		{
+			//Add Custom code here.
+			string message = $"No Intent";
+			await context.PostAsync(message);
+			//context.
+			context.Wait(MessageReceived);
+		}
+
+		//Will run when a GetWeather intent is Triggered
+		[LuisIntent("GetWeather")]
+		public async Task GetWeather(IDialogContext context, LuisResult result)
+		{
+			//Add Custom code here.
+			string message = $"GetWeather Intent";
+			await context.PostAsync(message);
+			context.Wait(MessageReceived);
+		}
+
+		//Will run when a Bookflight intent is Triggered
+		[LuisIntent("BookFlight")]
+		public async Task BookFlight(IDialogContext context, LuisResult result)
+		{
+			//Add Custom code here.
+			string message = $"BookFlight Intent";
+			await context.PostAsync(message);
+			context.Wait(MessageReceived);
+		}
+	}
+}
---
+++ final/csharp/MyFirstBot/README.md
@@ -0,0 +1,15 @@
+# MyFirstBot
+
+As there is not currently a Bot Framework template for Visual Studio Mac, use this bot at your starting application for developing a C# bot on OSX. 
+
+- Open this solution in [Visual Studio for Mac Preview](https://www.visualstudio.com/vs/visual-studio-mac)
+- Restore all packages
+- Build and Run the application
+
+When the application launches, you may see a 404 Error `System.Web.HttpException. The resource cannot be found`. Don't worry about this for now as the application still works and this doesn't prevent you from completing the lab.
+
+Also notice your app is using port 8080. Keep a note of this as you will need to use this address in the bot emulator to communicate with your bot: `http://localhost:8080/api/messages`
+
+
+![Echo Bot](echobot.png)
+
